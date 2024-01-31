357ed02576bc9006a654ba9775f281572ec41f2c
AymaneImr <elamraniayman388@gmail.com>
2024-02-01 00:05:14 +0100

    second commit

--- app.py
+++ app.py
@@ -7,37 +7,38 @@ from settings.settings import third
 from channels.channel import fourth
 from flask_login import LoginManager
 from flask_migrate import Migrate
+import os
 
-app = Flask(__name__)
-app.secret_key = "lovely29"
-app.permanent_session_lifetime = timedelta(days=1)
-app.app_context().push()
+flask_app = Flask(__name__)
+flask_app.secret_key = "lovely29"
+flask_app.permanent_session_lifetime = timedelta(days=1)
+flask_app.app_context().push()
 
 
 
 UPLOAD_FOLDER = 'admin/static/images/'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gogeta_user:NxCxcGMG5oIVJMldkOJwANoJpKPUE1Id@dpg-cmtcskqcn0vc73b9hpa0-a/gogeta'
+flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+flask_app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 login_manager = LoginManager()
-login_manager.init_app(app)
+login_manager.init_app(flask_app)
 login_manager.login_view = 'app.login'
 @login_manager.user_loader
 def load_user(get_id):
     return users.query.get(int(get_id))
 
-db.init_app(app)
+db.init_app(flask_app)
 
-migrate = Migrate(app, db)
+migrate = Migrate(flask_app, db)
 
-app.register_blueprint(app, url_prefix='/admin')
-app.register_blueprint(third, url_prefix='/admin/settings')
-app.register_blueprint(fourth, url_prefix='/admin/channels')
+flask_app.register_blueprint(app, url_prefix='/admin')
+flask_app.register_blueprint(third, url_prefix='/admin/settings')
+flask_app.register_blueprint(fourth, url_prefix='/admin/channels')
 
 
 if __name__ == "__main__":
     db.create_all()
-    app.run(debug=True)
+    flask_app.run(debug=True)
